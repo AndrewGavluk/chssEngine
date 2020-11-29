e5065141090e38e969ba9ad397fdec577dec3d34
AndrewGavluk <gavl.andr96@gmail.com>
2020-11-29 14:30:00 -0800

    first label

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
-#include <QApplication>
+//#include <memory>
 
+#include <QApplication>
+#include <QLabel>
 
 int main (int argc, char** argv)
 {
     QApplication game(argc, argv);
+    //std::unique_ptr<>
+
+    QLabel *label = new QLabel("Hello World");
+    label->show();
+
     return game.exec();
 }
